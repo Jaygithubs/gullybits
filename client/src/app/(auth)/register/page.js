28981157d20ff3314e5413fea655fdb7ce49aebe
Jay Sharma <jaysharma89533@gmail.com>
2026-01-23 16:27:28 +0530

    Make whole files structure of client

---
+++ client/src/app/(auth)/register/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Do you want to register?</div>
+  )
+}
+
+export default page
---
+++ client/src/app/(auth)/verify-email/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Do you want to verify your email?</div>
+  )
+}
+
+export default page
