e210e2b144122deba188b43a3c9cb538c78ed774
Jay Sharma <jaysharma89533@gmail.com>
2026-01-25 13:11:55 +0530

    fix verify-email page VerifyEmailClient component code

--- client/src/components/auth/VerifyEmailClient.jsx
+++ client/src/components/auth/VerifyEmailClient.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
-import { authService } from "@/services/auth.service";
+import { authService } from "../../services/auth.service";
 
 export default function VerifyEmailClient() {
   const searchParams = useSearchParams();
