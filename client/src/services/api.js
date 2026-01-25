6a36b4769541ab2058dd97cfafa6993e1015795f
Jay Sharma <jaysharma89533@gmail.com>
2026-01-25 14:47:32 +0530

    change envorment variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken, removeToken } from "../utils/token";
 
 const api = axios.create({
-  baseURL: process.env.API_BASE_URL || "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });  
 
 api.interceptors.request.use(
